da5998fe4581dc3512ce4aebd65eba2a8c9d73cc
reza <rezasemyari1991@gmail.com>
2020-06-08 18:52:42 +0430

    add section info-country

--- jQuery.js
+++ jQuery.js
@@ -21,7 +21,16 @@ $(document).ready(function () {
             $('#flag').attr('src', response.flag);
             $('#call-code').html(
                 `<p class="call-code">${response.callingCodes}</p>`
-                
+            )
+            $('#info-country').html(`
+            <p>${response.name}</p>
+            <span><p>Native Name:</p><p>${response.nativeName}</p></span>
+            <span><p>Capital:</p><p>${response.capital}</p></span>
+            <span><p>Region:</p><p>${response.region}, ${response.subregion}</p></span>
+            <span><p>Population:</p><p>${response.population}</p></span>
+            <span><p>Languages:</p><p>${response.languages[0].name}</p></span>
+            <span><p>Timezone:</p><p>${response.timezones[0]}</p></span>
+            `  
             )
         })
     })
